9431109b6bd3e3b1348b9548c9ed71e12a80e693
Denys Tryfonov <trifonowden123@gmail.com>
2023-11-16 16:59:54 +0200

    Updates

---
+++ assets/FvoritePage-7c35e825.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-71844611.js";const t=()=>e.jsx("h1",{children:"FavoritePage"});export{t as default};
---
+++ assets/HomePage-40c82902.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-71844611.js";const r=()=>e.jsx("h1",{children:"HomePage"});export{r as default};

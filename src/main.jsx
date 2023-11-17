931a547df46202ed82a6a81dbfd5256378061153
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-11-18 01:41:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { Provider } from "react-redux";
+// import { store, persistor } from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <Provider store={store}>

11896f084db9b42525f516e6d7969573827cb412
TrifonovDenys <54479637+TrifonovDenys@users.noreply.github.com>
2023-11-17 23:39:59 +0000

    Deploying to gh-pages from @ TrifonovDenys/auto-rent-client@c073294ed658a7639a2e8ec4366e3a54d021c137 🚀

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import { contactReducer } from './contacts/contactsSlice';
-// import { filterReducer } from './contacts/filterSlice';
-// import { authReducer } from './auth/slice';
-// import { modalReducer } from './contacts/modalSlice';
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// // import storage from 'redux-persist/lib/storage';
+// // import { contactReducer } from './contacts/contactsSlice';
+// // import { filterReducer } from './contacts/filterSlice';
+// // import { authReducer } from './auth/slice';
+// // import { modalReducer } from './contacts/modalSlice';
 
-// const authPersistConfig = {
-//   key: 'auth',
-//   storage,
-//   whitelist: ['token'],
-// };
+// // const authPersistConfig = {
+// //   key: 'auth',
+// //   storage,
+// //   whitelist: ['token'],
+// // };
 
-export const store = configureStore({
-  reducer: {
-    // auth: persistReducer(authPersistConfig, authReducer),
-    // contacts: contactReducer,
-    // filter: filterReducer,
-    // modal: modalReducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-  devTools: process.env.NODE_ENV === 'development',
-});
+// export const store = configureStore({
+//   reducer: {
+//     // auth: persistReducer(authPersistConfig, authReducer),
+//     // contacts: contactReducer,
+//     // filter: filterReducer,
+//     // modal: modalReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+//   devTools: process.env.NODE_ENV === 'development',
+// });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);

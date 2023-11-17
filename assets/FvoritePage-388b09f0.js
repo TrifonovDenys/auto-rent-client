fa6a1fe29b11fda153d4a264b06d3ecdecee2a1a
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-11-18 01:02:01 +0200

    Updates

--- assets/FvoritePage-388b09f0.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-caa88e3d.js";const t=()=>e.jsx("h1",{children:"FavoritePage"});export{t as default};
---
+++ assets/FvoritePage-4ee1651d.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-ce62a51c.js";const t=()=>e.jsx("h1",{children:"FavoritePage"});export{t as default};

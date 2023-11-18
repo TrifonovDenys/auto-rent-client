fb781100c1fcb345306882dfa1d3b91ff9d569d0
Tryfonov Denys <54479637+TrifonovDenys@users.noreply.github.com>
2023-11-18 23:08:27 +0200

    Updates

--- assets/FavoritesPage-4252a868.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ac2c1a42.js";const t=()=>e.jsx("h1",{children:"FavoritesPage"});export{t as default};
---
+++ assets/HomePage-1343b88c.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-acdfa766.js";const r=()=>e.jsx("h1",{children:"Home"});export{r as default};

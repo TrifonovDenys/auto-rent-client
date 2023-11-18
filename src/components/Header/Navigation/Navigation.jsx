a1a29f5277d3db3cdb893e71c99aea6cf3537969
Denys Tryfonov <trifonowden123@gmail.com>
2023-11-18 12:07:28 +0200

    fix

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,44 +1,47 @@
-import { NavLink } from "react-router-dom";
-import css from "./Navigation.module.css";
+import { NavLink } from 'react-router-dom';
+import css from './Navigation.module.css';
 const Navigation = () => {
-  return (
-    <>
-      <nav className="flex gap-10">
-        <NavLink
-          style={({ isActive }) => ({
-            color: isActive ? "#ffffff" : "#3470FF",
-            backgroundColor: isActive ? "#3470FF" : "",
-            boxShadow: isActive ? "0px 4px 4px 0px rgba(0,0,0,0.75)" : "",
-          })}
-          to="/"
-          className={css.nav_link}>
-          Home
-        </NavLink>
+    return (
+        <>
+            <nav className='flex gap-10'>
+                <NavLink
+                    style={({ isActive }) => ({
+                        color: isActive ? '#ffffff' : '#3470FF',
+                        backgroundColor: isActive ? '#3470FF' : '',
+                        boxShadow: isActive ? '0px 4px 4px 0px rgba(0,0,0,0.75)' : '',
+                    })}
+                    to='/'
+                    className={css.nav_link}
+                >
+                    Home
+                </NavLink>
 
-        <NavLink
-          style={({ isActive }) => ({
-            color: isActive ? "#ffffff" : "#3470FF",
-            backgroundColor: isActive ? "#3470FF" : "",
-            boxShadow: isActive ? "0px 4px 4px 0px rgba(0,0,0,0.75)" : "",
-          })}
-          to="/catalog"
-          className={css.nav_link}>
-          Catalog
-        </NavLink>
+                <NavLink
+                    style={({ isActive }) => ({
+                        color: isActive ? '#ffffff' : '#3470FF',
+                        backgroundColor: isActive ? '#3470FF' : '',
+                        boxShadow: isActive ? '0px 4px 4px 0px rgba(0,0,0,0.75)' : '',
+                    })}
+                    to='/catalog'
+                    className={css.nav_link}
+                >
+                    Catalog
+                </NavLink>
 
-        <NavLink
-          style={({ isActive }) => ({
-            color: isActive ? "#ffffff" : "#3470FF",
-            backgroundColor: isActive ? "#3470FF" : "",
-            boxShadow: isActive ? "0px 4px 4px 0px rgba(0,0,0,0.75)" : "",
-          })}
-          to="/favorite"
-          className={css.nav_link}>
-          Favorite
-        </NavLink>
-      </nav>
-    </>
-  );
+                <NavLink
+                    style={({ isActive }) => ({
+                        color: isActive ? '#ffffff' : '#3470FF',
+                        backgroundColor: isActive ? '#3470FF' : '',
+                        boxShadow: isActive ? '0px 4px 4px 0px rgba(0,0,0,0.75)' : '',
+                    })}
+                    to='/favorites'
+                    className={css.nav_link}
+                >
+                    Favorite
+                </NavLink>
+            </nav>
+        </>
+    );
 };
 
 export default Navigation;

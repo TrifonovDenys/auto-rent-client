6e7e427d549e3345eda8eb49e7ebb5a8058686fd
Denys Tryfonov <trifonowden123@gmail.com>
2023-11-22 19:55:27 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,46 +1,56 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import css from "./Filter.module.css"
+import css from './Filter.module.css';
 
 const Filter = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        console.dir(e.target.brand.value);
-        console.dir(e.target.price.value);
-    };
-    return (
-        <div className='w-full flex justify-center items-center pt-[150px] '>
-            <Formik initialValues={{ brand: '', price: '' }}>
-            <Form onSubmit={handleSubmit} className='flex items-end gap-[18px]'>
-                <div className='flex flex-col'>
-                <label htmlFor='brand' className={css.lable}>Car brand</label>
-                <Field as='select' name='brand' id='brand' className={css.input}>
-                    <option value='' selected disabled hidden>
-                    Enter the text
-                    </option>
-                    {['red', "green", 'blue'].map((el, idx) => (
-                        <option key={idx} value={el}>{el}</option>
-                    ))} 
-                </Field>
-                </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                <div className='flex flex-col'>
-                <label htmlFor='price' className={css.lable}>Price/ 1 hour</label>
-                <Field as='select' name='price' id='price' className={css.input}>
-                    <option value='' selected disabled hidden>
-                        To $
-                    </option>
+    // console.dir(e.target.brand.value);
+    // console.dir(e.target.price.value);
+  };
+  return (
+    <div className='w-full flex justify-center items-center pt-[150px] '>
+      <Formik initialValues={{ brand: '', price: '' }}>
+        <Form onSubmit={handleSubmit} className='flex items-end gap-[18px]'>
+          <div className='flex flex-col'>
+            <label htmlFor='brand' className={css.lable}>
+              Car brand
+            </label>
+            <Field as='select' name='brand' id='brand' className={css.input}>
+              <option value='' selected disabled hidden>
+                Enter the text
+              </option>
+              {['red', 'green', 'blue'].map((el, idx) => (
+                <option key={idx} value={el}>
+                  {el}
+                </option>
+              ))}
+            </Field>
+          </div>
 
-                    {['30', "40", '50'].map((el, idx) => (
-                        <option key={idx} value={el}>{el}</option>
-                    ))} 
-                </Field>
-                </div>
-                <button type='submit' className={css.button}>Search</button>
-            </Form>
-        </Formik>
-        </div>
-    );
+          <div className='flex flex-col'>
+            <label htmlFor='price' className={css.lable}>
+              Price/ 1 hour
+            </label>
+            <Field as='select' name='price' id='price' className={css.input}>
+              <option value='' selected disabled hidden>
+                To $
+              </option>
+
+              {['30', '40', '50'].map((el, idx) => (
+                <option key={idx} value={el}>
+                  {el}
+                </option>
+              ))}
+            </Field>
+          </div>
+          <button type='submit' className={css.button}>
+            Search
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default Filter;

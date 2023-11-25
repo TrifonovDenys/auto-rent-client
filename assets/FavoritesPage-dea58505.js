7b67ad2bdd5f5f807704a753e14843c9e54fb4b6
TrifonovDenys <54479637+TrifonovDenys@users.noreply.github.com>
2023-11-25 12:25:56 +0000

    Deploying to gh-pages from @ TrifonovDenys/auto-rent-client@cd171ff7fa7a07a42178bc8247be5d0bc89ad557 🚀

--- assets/FavoritesPage-dea58505.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-b078cccc.js";const t=()=>e.jsx("h1",{children:"FavoritesPage"});export{t as default};
--- assets/HomePage-e61b1f3c.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-b078cccc.js";const r=()=>e.jsx("h1",{children:"Home"});export{r as default};

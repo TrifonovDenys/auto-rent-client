86fb675a158056f6052158bf474855e6c7e62d1a
Denys Tryfonov <trifonowden123@gmail.com>
2023-11-25 11:26:29 +0200

    Update selectors.js

--- src/redux/cars/selectors.js
+++ src/redux/cars/selectors.js
@@ -1,12 +1,4 @@
-<<<<<<< Updated upstream
-export const getFilter = state => state.filter.filter;
-
-export const getModal = state => state.modal.isOpen;
-
-export const getCar = state => state.modal.currentCar;
-=======
 export const getFilter = (state) => state.filter.filter;
 export const getModal = (state) => state.modal.isOpen;
 export const getCar = (state) => state.modal.currentCar;
 export const getFavorites = (state) => state.favorites.favorites;
->>>>>>> Stashed changes

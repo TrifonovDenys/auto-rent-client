96e820f0431af84196f2fea9d099f0100f2665cc
Denys Tryfonov <trifonowden123@gmail.com>
2023-11-27 12:10:30 +0200

    Update CatalogPage.jsx

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -9,72 +9,89 @@ import { useSearchParams } from 'react-router-dom';
 import { useEffect, useMemo, useState } from 'react';
 
 const CatalogPage = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [favorites, setFavorites] = useState([]);
-    const [lCount, setLCount] = useState(12);
-    
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [favorites, setFavorites] = useState([]);
+  const [lCount, setLCount] = useState(12);
+  const [loadMore, setLoadMore] = useState(true);
 
-    const params = useMemo(
-        () => Object.fromEntries([...searchParams]),
-        [searchParams]
-      );
+  const isOpen = useSelector(getModal);
+  const brandF = useSelector(getBrandFilter);
+  const priceF = useSelector(getPriceFilter);
 
-    const { limit, totalHits = 32 } = params;
-    const {data: fetchedCars} = useGetCarsQuery({ limit, totalHits });
+  const params = useMemo(() => Object.fromEntries([...searchParams]), [searchParams]);
 
-    useEffect(() => {
-        if (!fetchedCars) {
-          return;
-        }
+  const { limit, totalHits = 32 } = params;
+  const { data: fetchedCars } = useGetCarsQuery({ limit, totalHits });
 
-        setSearchParams({
-          ...params,
-          limit: lCount,
-          totalHits
-        });
-    
-        setFavorites(fetchedCars);
-      }, [fetchedCars, lCount, params, setSearchParams]);
-
-    const isOpen = useSelector(getModal)
-    const brandF = useSelector(getBrandFilter)
-    const priceF = useSelector(getPriceFilter)
-
-    let filteredCar = favorites
-    if (brandF){
-        filteredCar = filteredCar.filter(el=>el.make.includes(brandF))
+  useEffect(() => {
+    if (!fetchedCars) {
+      return;
     }
-    if(priceF){
-        filteredCar = filteredCar.filter(el=>el.rentalPrice === "$" + priceF)
+
+    if (brandF || priceF) {
+      setSearchParams({
+        ...params,
+        limit: totalHits,
+        totalHits,
+      });
+    } else {
+      setSearchParams({
+        ...params,
+        limit: lCount,
+        totalHits,
+      });
     }
-    const lm = totalHits / lCount > 1
-    return (
-        <>
-        {isOpen && <Modal />}
-            <Filter />
-            <ul className={css[`Car_list`]}>
-                {filteredCar.length > 0 ? filteredCar.map((car) => {
-                    return (
-                        <li className={css[`Car_item`]} key={car.id}>
-                            <CarCard car={car} />
-                        </li>
-                    );
-                }): <p>No cars</p>}
-            </ul>
-            {lm ? 
-            <div className=' w-full flex justify-center pb-[150px]'>
-                <button onClick={()=> {setLCount(limit=> {
-                    if(limit + 12 > totalHits){
-                        return totalHits
-                    }
-                    return limit + 12})}} type='button' className='text-[#3470FF] hover:text-[#0B44CD] underline  border-none'>
-                    Load more
-                </button>
-            </div> : <></>} 
-        </>
-    );
+    setLoadMore(totalHits / limit > 1);
+    setFavorites(fetchedCars);
+  }, [fetchedCars, lCount, params, setSearchParams, brandF, priceF, setLoadMore]);
+
+  let filteredCar = favorites;
+  if (brandF) {
+    filteredCar = filteredCar.filter((el) => el.make.includes(brandF));
+  }
+  if (priceF) {
+    filteredCar = filteredCar.filter((el) => el.rentalPrice === '$' + priceF);
+  }
+
+  return (
+    <>
+      {isOpen && <Modal />}
+      <Filter />
+      <ul className={css[`Car_list`]}>
+        {filteredCar.length > 0 ? (
+          filteredCar.map((car) => {
+            return (
+              <li className={css[`Car_item`]} key={car.id}>
+                <CarCard car={car} />
+              </li>
+            );
+          })
+        ) : (
+          <p>No cars</p>
+        )}
+      </ul>
+      {loadMore ? (
+        <div className=' w-full flex justify-center pb-[150px]'>
+          <button
+            onClick={() => {
+              setLCount((limit) => {
+                if (limit + 12 > totalHits) {
+                  return totalHits;
+                }
+                return limit + 12;
+              });
+            }}
+            type='button'
+            className='text-[#3470FF] hover:text-[#0B44CD] underline  border-none'
+          >
+            Load more
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 export default CatalogPage;
-
-
